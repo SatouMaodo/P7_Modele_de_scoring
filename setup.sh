87c6d42dc5326f9340567966b1c0403543a33241
SatouMaodo <amsatoun504@gmail.com>
2025-01-21 16:36:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export MLFLOW_TRACKING_URI=https://d483-34-106-155-85.ngrok-free.app
+export MLFLOW_TRACKING_URI=https://f88e-34-138-30-203.ngrok-free.app"
 # Remplacez par l'URI de votre serveur MLflow si nécessaire
 mlflow models serve -m models:/Scoring_model/y -h 0.0.0.0 -p 8080 &
