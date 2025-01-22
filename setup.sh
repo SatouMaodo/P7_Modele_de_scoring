70a786f13866935796c985e18d9ad58d4ff8d0b6
SatouMaodo <amsatoun504@gmail.com>
2025-01-22 22:34:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
-export MLFLOW_TRACKING_URI="sqlite:///mlflow.db"
-# Remplacez par l'URI de votre serveur MLflow si nécessaire
-mlflow models serve -m models:/mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/y -h 127.0.0.1 -p 3313 &
+export MLFLOW_TRACKING_URI="http://127.0.0.1:3313"
+
