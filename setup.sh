1ede1429093a2ec2cf3f3e5cbbaf8840bada181e
SatouMaodo <amsatoun504@gmail.com>
2025-01-22 19:26:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # export MLFLOW_TRACKING_URI="http://votre-serveur-mlflow:5000" 
 
 # Définissez le chemin du modèle (utilisez le chemin relatif ou absolu)
-MODEL_PATH="/mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/"
+MODEL_PATH="/mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts"
 
 # Démarrez le serveur de modèle MLflow 
 mlflow models serve -m "$MODEL_PATH" -h 0.0.0.0 -p 5313 &
