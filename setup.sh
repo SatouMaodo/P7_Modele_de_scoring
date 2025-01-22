38ac30ca55c3933304509cd327e26518b709d2c9
SatouMaodo <amsatoun504@gmail.com>
2025-01-22 20:10:13 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Définissez l'URI de suivi MLflow (si vous utilisez un serveur MLflow)
-# export MLFLOW_TRACKING_URI="http://votre-serveur-mlflow:5000" 
+export MLFLOW_TRACKING_URI="http://127.0.0.1:5000" 
 
 # Définissez le chemin du modèle (utilisez le chemin relatif ou absolu)
 MODEL_PATH="mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/Scoring_model"
