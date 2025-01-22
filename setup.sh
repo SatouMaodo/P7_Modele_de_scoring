690e0aca69cdcb36f07de4f31307b341436f3ec2
SatouMaodo <amsatoun504@gmail.com>
2025-01-22 19:45:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@
 MODEL_PATH="/mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/Scoring_model"
 
 # Démarrez le serveur de modèle MLflow 
-mlflow models serve -m "$MODEL_PATH" -h 0.0.0.0 -p 5313 &
+mlflow models serve -m "$MODEL_PATH" -h 0.0.0.0 -p 8081 &
