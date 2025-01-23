817340446846d44fd98119f175d1f19b16404e28
SatouMaodo <amsatoun504@gmail.com>
2025-01-23 21:49:02 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 # Remplacez par l'URI de votre serveur MLflow si nécessaire
 #mlflow models serve -m models:file:///mlruns -h 0.0.0.0 -p 8080 &
 # Démarrer le serveur de modèle MLflow
-mlflow models serve -m "runs:/e168c89c821b4680a3a92d7ee0ed2e28/mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/Scoring_model" -h 0.0.0.0 -p 8080 &
+mlflow models serve -m "runs:/e168c89c821b4680a3a92d7ee0ed2e28/Scoring_model" -h 0.0.0.0 -p 8080 &
