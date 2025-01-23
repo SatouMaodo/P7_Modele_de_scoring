4e69d8f589c2e957dd78b145b1c405666126831f
SatouMaodo <amsatoun504@gmail.com>
2025-01-23 13:01:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export MLFLOW_TRACKING_URI="mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/"
+export MLFLOW_TRACKING_URI="mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/Scoring_model"
 # Remplacez par l'URI de votre serveur MLflow si nécessaire
-mlflow models serve -m models:mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/ -h 127.0.0.1 -p 8080 &
+mlflow models serve -m models:mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts/Scoring_model -h 127.0.0.1 -p 8080 &
