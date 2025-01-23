1059e886931210bf6f5a34b2db80797c0899fc5f
SatouMaodo <amsatoun504@gmail.com>
2025-01-23 14:30:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export MLFLOW_TRACKING_URI="http://127.0.0.1:5313"
+export MLFLOW_TRACKING_URI="sqlite:///mlflow.db"
 # Remplacez par l'URI de votre serveur MLflow si nécessaire
-mlflow models serve -m models:https://98fd-34-168-184-236.ngrok-free.app -h 0.0.0.0 -p 8080 &
+mlflow models serve -m models:sqlite:///mlflow.db -h 0.0.0.0 -p 8080 &
