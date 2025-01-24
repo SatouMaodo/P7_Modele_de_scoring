c65e818fc9352aab922e5169c241a676f531daba
SatouMaodo <amsatoun504@gmail.com>
2025-01-24 13:11:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-export MLFLOW_TRACKING_URI="./mlruns"
-export MLFLOW_MODEL_URI="models:/Projet7_Scoring_model/v1"
+export MLFLOW_TRACKING_URI="mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts"
+export MLFLOW_MODEL_URI="models:/Scoring_model"
 #export MLFLOW_TRACKING_URI="https://5451-34-16-135-108.ngrok-free.app"
 # Remplacez par l'URI de votre serveur MLflow si nécessaire
 #mlflow models serve -m models:file:///mlruns -h 0.0.0.0 -p 8080 &
 # Démarrer le serveur de modèle MLflow
 #mlflow models serve -m "runs:/e168c89c821b4680a3a92d7ee0ed2e28/Scoring_model" -h 0.0.0.0 -p 8080 &
 
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-pip install mlflow
 
 
 # Définissez les variables d'environnement pour votre application
