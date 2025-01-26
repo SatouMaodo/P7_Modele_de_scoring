98aca18e3806f28db1522d6b1000d6d45af66f1a
SatouMaodo <amsatoun504@gmail.com>
2025-01-26 14:01:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-export MLFLOW_TRACKING_URI="sqlite:///mlflow.db" 
-#export MLFLOW_ARTIFACT_ROOT=mlruns/1/e168c89c821b4680a3a92d7ee0ed2e28/artifacts
-#export MLFLOW_MODEL_URI="models:/Scoring_model"
-#export MLFLOW_TRACKING_URI="https://5451-34-16-135-108.ngrok-free.app"
-# Remplacez par l'URI de votre serveur MLflow si nécessaire
-#mlflow models serve -m models:file:///mlruns -h 0.0.0.0 -p 8080 &
-# Démarrer le serveur de modèle MLflow
-#mlflow models serve -m "runs:/e168c89c821b4680a3a92d7ee0ed2e28/Scoring_model" -h 0.0.0.0 -p 8080 &
-
-
-
-# Définissez les variables d'environnement pour votre application
-#export MLFLOW_MODEL_PATH='models:/Projet7_Scoring_model/Production'
-
+mkdir -p mlruns
