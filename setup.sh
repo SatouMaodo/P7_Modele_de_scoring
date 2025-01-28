0c0f69d8430a12aaffac8b3f04feef4bd6fed9bf
SatouMaodo <amsatoun504@gmail.com>
2025-01-28 15:02:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 export MLFLOW_TRACKING_URI=file:///mlflow.db
-mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts --host 0.0.0.0 --port 5000 &
+mlflow server --backend-store-uri file:///mlflow.db --default-artifact-root ./artifacts --host 0.0.0.0 --port 5000 &
