613491e4d6b13f5342dfeca233070d1aedcb8ea2
SatouMaodo <amsatoun504@gmail.com>
2025-01-28 15:14:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
-export MLFLOW_TRACKING_URI=file:///mlflow.db
-mlflow server --backend-store-uri file:///mlflow.db --default-artifact-root ./artifacts --host 0.0.0.0 --port 5000 &
+#!/bin/bash
+
+# Example setup script
+echo "Setting up your Heroku environment..."
+
+# Install necessary dependencies (if any)
+pip install -r requirements.txt
+
+# Set up SQLite database for MLflow
+echo "Setting up SQLite database..."
+touch mlflow.db  # Crée la base de données si elle n'existe pas encore.
+
+# You can add other necessary setup steps here
