f6f2e5d405957b693d7f1387d339984dce26ab43
SatouMaodo <amsatoun504@gmail.com>
2025-01-30 17:44:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Définissez l'URL publique ngrok de votre interface utilisateur MLflow
-mlflow_ui_url = "https://d64d-104-197-221-124.ngrok-free.app"
+mlflow_ui_url = "https://bca8-34-74-70-222.ngrok-free.app"
 
 # Affichez l'interface utilisateur MLflow dans un iframe Streamlit
 st.title("Comparaison des modèles:Projet 7-Implémenter un modèle de scoring")
