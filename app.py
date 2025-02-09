dba520c48225adf405afb5356e09b8a551e3af30
SatouMaodo <amsatoun504@gmail.com>
2025-02-09 10:18:48 +0100

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Définissez l'URL publique ngrok de mon interface utilisateur MLflow
-mlflow_ui_url = "https://f5e9-34-126-158-9.ngrok-free.app"
-
-# Affichez l'interface utilisateur MLflow dans un iframe Streamlit
-st.title("Comparaison des modèles: P7-Implémenter un modèle de scoring")
-st.components.v1.iframe(mlflow_ui_url, width=1500, height=1000)
