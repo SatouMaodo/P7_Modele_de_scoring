ba9eb831aaaaddea88f281e10981cee3aa71c060
SatouMaodo <amsatoun504@gmail.com>
2025-02-10 15:13:55 +0100

    Delete Tests/tests directory

--- Tests/tests/test_code.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from my_functions import app
-test_client=TestClient(app)
-
-
-def test_predict():
-    reponse=test_client.post("/predict", json={"client_id":100001})
-    assert reponse.status_code==200
-    assert reponse.json()['prediction']==0.060
