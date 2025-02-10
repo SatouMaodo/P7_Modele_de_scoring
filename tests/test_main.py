0e94892f57dfbd7163b5c00858c4a12168032bee
SatouMaodo <amsatoun504@gmail.com>
2025-02-10 15:24:01 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,13 @@
 from fastapi.testclient import TestClient
+import sys
+import os
+
+# Obtenir le répertoire du fichier courant
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Ajouter le répertoire parent au chemin de recherche des modules
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 from main import app
 test_client=TestClient(app)
 
