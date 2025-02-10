01782dc62c01903b4a1a5a73af71200d80aad471
SatouMaodo <amsatoun504@gmail.com>
2025-02-10 15:53:01 +0100

    Update test_main.py

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -15,7 +15,12 @@ from main import app
 test_client=TestClient(app)
 
 
+from fastapi.testclient import TestClient
+from main import app
+test_client=TestClient(app)
+
+
 def test_predict():
-    reponse = test_client.post("/predict", json={"client_id": str(100001)}) 
+    reponse=test_client.post("/predict", json={"client_id":100001})
     assert reponse.status_code==200
     assert reponse.json()['prediction']==0.060
