a822ed230ad068300e02310fb7c30560ef9be7e5
SatouMaodo <amsatoun504@gmail.com>
2025-02-10 15:15:26 +0100

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from main import app
+test_client=TestClient(app)
+
+
+def test_predict():
+    reponse=test_client.post("/predict", json={"client_id":100001})
+    assert reponse.status_code==200
+    assert reponse.json()['prediction']==0.060
